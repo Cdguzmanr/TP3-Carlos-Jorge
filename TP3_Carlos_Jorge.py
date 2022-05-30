e93dda671a62a7d3708c9ce436b48d521905115a
Carlos Guzmán Ramírez <cguzmanr17@gmail.com>
2022-05-30 17:20:34 -0600

    Creación del Main

---
+++ TP3_Carlos_Jorge.py
@@ -0,0 +1,10 @@
+#######################################################
+#Creado por: Carlos Guzmán, Jorge Guevara
+#Fecha de creación: 30/05/2022 5:15 pm
+#Última modificación: 30/05/2022 00:00 pm 
+#Versión de python: 3.10.2
+#######################################################
+
+# Definición de funciones
+
+# Programa Principal (PP)
