d2ecd0d89543cd66758c869ec32c5a529f0c6865
Carlos Guzmán Ramírez <cguzmanr17@gmail.com>
2022-05-31 17:44:34 -0600

    Ventana Principal
    Se creó la ventana principal del programa, y vinculé las funciones base a sus botones respectivos

--- TP3_Carlos_Jorge.py
+++ TP3_Carlos_Jorge.py
@@ -1,34 +1,99 @@
 #######################################################
 #Creado por: Carlos Guzmán, Jorge Guevara
 #Fecha de creación: 30/05/2022 5:15 pm
-#Última modificación: 30/05/2022 00:00 pm 
+#Última modificación: 31/05/2022 4:50 pm 
 #Versión de python: 3.10.2
 #######################################################
+
+# Importación de Librerias
 from tkinter import *
 
+###################################################################################
 # Definición de funciones
-    # Cargar países
+
+    #Funnciones Generales
+def refrescarVentana(ventana, comando):
+    """
+    Funcionamiento: Permite refrescar una ventana
+    Entrada: ventana (ventana a cerrar), comando (función a llamar)
+    """
+    ventana.destroy()
+    comando()
+
+def crearAviso(msg, ventanaPrincipal):
+    """
+    Funcionamiento: Permite crear una ventana de aviso
+    Entrada: msg (mensaje de aviso), ventanaPrincipal (Ventana donde se devuelve al usuario)   
+    """
+    ventanaAviso = Toplevel(ventanaPrincipal)
+    ventanaAviso.grab_set()
+    ventanaAviso.title('Aviso')
+    ventanaAviso.geometry('500x100')
+    ventanaAviso.configure(bg='white')
+    encabezadoAviso = Label(ventanaAviso, text=msg, font=("Calibri 12"),bg='white')
+    encabezadoAviso.pack()
+    botonSalir = Button(ventanaAviso, text="Entendido", width=40, height=3, bg='lightblue', command= lambda:ventanaAviso.destroy())
+    botonSalir.pack()
 
 
-#_________________________________________________________________________________# 
+###################################################################################
+# Funciones Principales
+
+#_________________________________________________________________________________# Boton 1
+def cargarBD(): 
+    print("\nCargar bases de datos")
+    return
+
+#_________________________________________________________________________________# Boton 2
+def registrarUsuario(): 
+    print("\nRegistrar nuevo usuario")
+    return
+
+#_________________________________________________________________________________# Boton 3
+def registrarDinamico(): 
+    print("\nRegistro dinámico")
+    return
+
+#_________________________________________________________________________________# Boton 4
+def modificarUsuario(): 
+    print("\nModificar información de usuario")
+    return
+
+#_________________________________________________________________________________# Boton 5
+def eliminarUsuario(): 
+    print("\nEliminar usuario")
+    return
+
+#_________________________________________________________________________________# Boton 6
+def exportarXML(): 
+    print("\nExportar XML")
+    return
+
+#_________________________________________________________________________________# Boton 7
+def reportes(): 
+    print("\nGenerar Reportes")
+    return
+
+
+###################################################################################
 # Interfaz gráfica
-ventana = Tk()
-ventana.geometry("600x700")
-ventana.title("PAMTEC")
-ventana.configure(bg='white')
-
+inicio = Tk()
+inicio.geometry("500x600")
+inicio.resizable(False, False)
+inicio.title("Equipo de trabajo MBTI")
+inicio.config(bg="white")
 # Encabezado
-titulo = Label(ventana, text="Adoptemos un adulto mayor", font=("Calibri 20"),bg='white')
+titulo = Label(inicio, text="Equipo de trabajo MBTI", font=("Calibri 20"),bg='white')
 titulo.grid(row=0, column=0, padx=80, pady=10)
 # Botones
-boton1 = Button(ventana, text="Cargar BD de países", width=65, height=3, bg='lightblue')
-boton2 = Button(ventana, text="Insertar un participante", width=65, height=3, bg='lightblue')
-boton3 = Button(ventana, text="Insertar n participantes", width=65, height=3, bg='lightblue')
-boton4 = Button(ventana, text="Enlazar con abuelos", width=65, height=3, bg='lightblue')
-boton5 = Button(ventana, text="Dar de baja", width=65, height=3, bg='lightblue')
-boton6 = Button(ventana, text="Escribe una carta a su correo", width=65, height=3, bg='lightblue')
-boton7 = Button(ventana, text="Reportes", width=65, height=3, bg='lightblue')
-boton8 = Button(ventana, text="Salir", width=65, height=3, bg='lightblue', command=ventana.destroy) # "ventana.destroy" Permite cerrar la ventana
+boton1 = Button(inicio, text="Cargar Bases de Datos", width=65, height=3, bg="#deb1bf", command=cargarBD)
+boton2 = Button(inicio, text="Registrar usuario nuevo", width=65, height=3, bg='#deb1bf', command=registrarUsuario)
+boton3 = Button(inicio, text="Registro dinámico", width=65, height=3, bg='#b8daba', command=registrarDinamico)
+boton4 = Button(inicio, text="Modificar datos de usuario", width=65, height=3, bg='#b8daba', command=modificarUsuario)
+boton5 = Button(inicio, text="Eliminar usuario", width=65, height=3, bg='#c5e2f6', command=eliminarUsuario)
+boton6 = Button(inicio, text="Exportar XML", width=65, height=3, bg='#c5e2f6', command=exportarXML) 
+boton7 = Button(inicio, text="Reportes", width=65, height=3, bg='#ffffbf', command=reportes)
+boton8 = Button(inicio, text="Salir", width=65, height=3, bg='#ffffbf', command=inicio.destroy) # "inicio.destroy" Permite cerrar la inicio
 # Edición de los botones
 boton1.grid(row=1, column=0, padx=20, pady=5)
 boton2.grid(row=2, column=0, padx=20, pady=5)
@@ -39,7 +104,7 @@ boton6.grid(row=6, column=0, padx=20, pady=5)
 boton7.grid(row=7, column=0, padx=20, pady=5)
 boton8.grid(row=8, column=0, padx=20, pady=5)
 
-# Programa principal
+# Programa principal (PP)
 mainloop()
 
 
